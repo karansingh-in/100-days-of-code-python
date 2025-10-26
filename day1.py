bb5dcfdee7d12a4cbbab0cf564e1bb1641ab1904
Karan <singh.karangkp@gmail.com>
2025-10-26 08:27:24 +0000

    day 1

---
+++ day1.py
@@ -0,0 +1,8 @@
+print('Welcome to brand name generator')
+print('what city did you grew up in?')
+city = input()
+
+print('what\'s the name of your pet?')
+pet = input()
+
+print('Your brand name could be '+city + " " + pet)
