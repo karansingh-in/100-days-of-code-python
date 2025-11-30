a384e301cfafa2bd6512272b8a7758b7e72b8623
Karan <singh.karangkp@gmail.com>
2025-11-30 08:14:40 +0000

    it can turn now

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -11,6 +11,7 @@ screen.tracer(0) # stops auto refresh of screen
 
 
 segments = []
+
 for position in starting_position:
     new_part = t.Turtle()
     new_part.shape('square')
@@ -20,14 +21,42 @@ for position in starting_position:
     segments.append(new_part)
 screen.update() # force screen refresh
 
+def turn_left():
+    i = int(0)
+    while i<len(segments):
+        segments[i].forward(20*(i+1))
+        screen.update()
+        time.sleep(0.1)
+        j = i
+        while not (j - 1 < 0):
+            segments[j-1].forward(20)
+            screen.update()
+            time.sleep(0.1)
+            j -= 1
+        segments[i].left(90)
+        i += 1
+
+
+for parts in segments:
+    parts.forward(50)
+    screen.update()
+    time.sleep(0.1)
+    
+turn_left()
+# screen.update()
+# time.sleep(0.1)
 
 game_running = True
 while game_running:
     for parts in segments:
         parts.forward(20)
-        time.sleep(1)
-    screen.update()
 
+    screen.update() # when all the parts move a step forward the screen refreshes 
+    time.sleep(0.1) # this makes the screen refresh delay time to 0.1 sec so the snake looks faster 
+    
 
 
 screen.exitonclick()
+
+
+
