a54cdd26a5973a498ba80070257ebd4aeec81d16
Karan <singh.karangkp@gmail.com>
2025-11-30 07:33:41 +0000

    the snake moves forward now

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -1,4 +1,5 @@
 import turtle as t
+import time
 
 screen = t.Screen()
 
@@ -6,6 +7,8 @@ screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('Snake Game')
 starting_position = [(0,0), (-20,0), (-40,0)]
+screen.tracer(0) # stops auto refresh of screen
+
 
 segments = []
 for position in starting_position:
@@ -15,12 +18,15 @@ for position in starting_position:
     new_part.penup()
     new_part.goto(position)
     segments.append(new_part)
+screen.update() # force screen refresh
+
 
 game_running = True
 while game_running:
     for parts in segments:
         parts.forward(20)
-
+        time.sleep(1)
+    screen.update()
 
 
 
