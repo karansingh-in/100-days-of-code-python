58ffb70fa6403776e4b516d9bc78f47680a04d9b
Karan <singh.karangkp@gmail.com>
2025-12-01 16:46:25 +0000

    added score board

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -3,7 +3,11 @@ import time
 from snake import Snake
 from food import Food
 screen = t.Screen()
+score = t.Turtle()
+score.color('white')
+score.penup()
 
+score.ht()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('Snake Game')
@@ -11,11 +15,15 @@ screen.tracer(0)
 saap = Snake()
 khana = Food()
 
+scorecard = int(0)
+score.goto(0,280)
+score.write(f"Score: {scorecard}",  align='center', font=(8))
+
 #khana.__init__()
 khana.place_food()
 game_running = True
 while game_running:
-    
+
     position_of_snake = saap.segments[0].pos()
     print(position_of_snake, khana.position_of_food)
     
@@ -24,7 +32,11 @@ while game_running:
         new_tail = ((multiplier * -10), 0)
         print('kha liyeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')
         khana.place_food()
-        
+        score.clear()
+        score.goto(0,280)
+        scorecard += 1
+
+        score.write(f"Score: {scorecard}", align='center', font=(8))
         saap.starting_position.append(new_tail)
     saap.move() # the fucntion should has 'self' in it s decleration else it throws an error that 0 arguments were expected but 1 was given because self argument is given by default to the function whenever the function is called
     screen.update() # when all the parts move a step forward the screen refreshes 
