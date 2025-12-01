087e06e0e06b5cb12f595a16d6fd21a123555c8e
Karan <singh.karangkp@gmail.com>
2025-12-01 15:26:38 +0000

    added food function

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -1,6 +1,7 @@
 import turtle as t
 import time
 from snake import Snake
+from food import Food
 screen = t.Screen()
 
 screen.setup(width=600, height=600)
@@ -8,17 +9,33 @@ screen.bgcolor('black')
 screen.title('Snake Game')
 screen.tracer(0)
 saap = Snake()
+khana = Food()
 
-
+#khana.__init__()
+khana.place_food()
 game_running = True
 while game_running:
     
-
+    position_of_snake = saap.segments[0].pos()
+    print(position_of_snake, khana.position_of_food)
+    
+    if(int(position_of_snake[0]) == int(khana.position_of_food[0]) and int(position_of_snake[1]) == int(khana.position_of_food[1])):
+        #multiplier = int(len(saap.starting_position))
+        print('kha liyeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')
+        khana.place_food()
+        #saap.starting_position.append()
     saap.move() # the fucntion should has 'self' in it s decleration else it throws an error that 0 arguments were expected but 1 was given because self argument is given by default to the function whenever the function is called
     screen.update() # when all the parts move a step forward the screen refreshes 
-    time.sleep(0.1) # this makes the screen refresh delay time to 0.1 sec so the snake looks faster 
-    
+    time.sleep(0.1) # this makes the screen refresh delay time to 0.1 sec so the snake looks faster
 
+
+    screen.listen()
+    screen.onkey(saap.up, 'Up')
+    screen.onkey(saap.down, 'Down')
+    screen.onkey(saap.left, 'Left')
+    screen.onkey(saap.right, 'Right')
+
+   
 
 screen.exitonclick()
 
--- Snake Game/snake.py
+++ Snake Game/snake.py
@@ -1,32 +1,57 @@
 import turtle as t
+import time
+
 
 
 class Snake():
     screen = t.Screen()
-    starting_position = [(0,0), (-20,0), (-40,0)]
-   # screen.tracer(0) # stops auto refresh of screen
-
-    segments = []
-
-    for position in starting_position:
-        new_part = t.Turtle()
-        new_part.shape('square')
-        new_part.color('white')
-        new_part.penup()
-        new_part.goto(position)
-        segments.append(new_part)
-    screen.update() # force screen refresh
+    def __init__(self):
+        self.starting_position = [(0,0), (-20,0), (-40,0)]            
+        self.segments = []
+        for position in self.starting_position:
+            new_part = t.Turtle()
+            new_part.shape('square')
+            new_part.color('pink')
+            new_part.penup()
+            new_part.goto(position)
+            self.segments.append(new_part)
+        self.screen.update() # force screen refresh
+        time.sleep(0.1)
 
     def move(self):
-        segments = Snake.segments
-        screen = Snake.screen
-        for i in range(len(segments) - 1, 0, -1): # in range(start, end, step)
-            past_x = segments[i-1].xcor() # x coordiante of the last block
-            past_y = segments[i-1].ycor() # y coordinate of the last block
-            segments[i].goto(past_x, past_y) # new segment moving to the position of old block
-        segments[0].forward(20) # moves block one forward, the rest follow along
-        #segments[0].left(90)
-        
-        screen.listen()
-        screen.onkey(segments[0].left(90), 'a')
-        screen.onkey(segments[0].left(270), 'd')
+        for i in range(len(self.segments) - 1, 0, -1): # in range(start, end, step)
+            past_x = self.segments[i-1].xcor() # x coordiante of the last block
+            past_y = self.segments[i-1].ycor() # y coordinate of the last block
+            self.segments[i].goto(past_x, past_y) # new segment moving to the position of old block
+        self.segments[0].forward(20) # moves block one forward, the rest follow along
+    def up(self):
+        for i in range(len(self.segments) - 1, 0, -1): # in range(start, end, step)
+            past_x = self.segments[i-1].xcor() # x coordiante of the last block
+            past_y = self.segments[i-1].ycor() # y coordinate of the last block
+            self.segments[i].goto(past_x, past_y) # new segment moving to the position of old block
+        self.segments[0].setheading(0)
+        self.segments[0].left(90) # moves block one forward, the rest follow along   
+    def down(self):
+        for i in range(len(self.segments) - 1, 0, -1): # in range(start, end, step)
+            past_x = self.segments[i-1].xcor() # x coordiante of the last block
+            past_y = self.segments[i-1].ycor() # y coordinate of the last block
+            self.segments[i].goto(past_x, past_y) # new segment moving to the position of old block
+        self.segments[0].setheading(0)
+        self.segments[0].left(270) # moves block one forward, the rest follow along
+    def left(self):
+        for i in range(len(self.segments) - 1, 0, -1): # in range(start, end, step)
+            past_x = self.segments[i-1].xcor() # x coordiante of the last block
+            past_y = self.segments[i-1].ycor() # y coordinate of the last block
+            self.segments[i].goto(past_x, past_y) # new segment moving to the position of old block
+        self.segments[0].setheading(0)
+        self.segments[0].left(180) # moves block one forward, the rest follow along
+    def right(self):
+        for i in range(len(self.segments) - 1, 0, -1): # in range(start, end, step)
+            past_x = self.segments[i-1].xcor() # x coordiante of the last block
+            past_y = self.segments[i-1].ycor() # y coordinate of the last block
+            self.segments[i].goto(past_x, past_y) # new segment moving to the position of old block
+        self.segments[0].setheading(0)
+        #self.segments[0].left(270) # moves block one forward, the rest follow along
+            
+            
+            
