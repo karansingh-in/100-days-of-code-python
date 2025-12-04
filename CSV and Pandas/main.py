29088c045bf47d72f5f8cbabdb21980c7f52f3b7
Karan <singh.karangkp@gmail.com>
2025-12-04 18:00:22 +0000

    worked with dataframes

--- CSV and Pandas/main.py
+++ CSV and Pandas/main.py
@@ -1,8 +1,28 @@
-import csv
+import pandas as pd
+data = pd.read_csv('./2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv')
 
-# data = []
-# with open('./weather_data.csv') as file:
-#     for lines in file:
-#         data.append(lines)
-        
-# print(data)
+grey_sq = data[data['Primary Fur Color'] == 'Gray']
+cinnamon_sq = data[data['Primary Fur Color'] == 'Cinnamon']
+black_sq = data[data['Primary Fur Color'] == 'Black']
+
+
+grey_count = grey_sq.count()
+cinnamon_count = cinnamon_sq.count()
+black_count = black_sq.count()
+
+
+new_data = {
+    'Fur color' : ['Gray', 'Cinnamon', 'Black'],
+    'count' : [grey_count.Y, cinnamon_count.Y, black_count.Y]
+}
+
+df = pd.DataFrame(new_data)
+
+df.to_csv('squirrel_count.csv', index=False)
+
+
+# print(grey_sq)
+# print(cinnamon_sq)
+# print(black_sq)
+
+
